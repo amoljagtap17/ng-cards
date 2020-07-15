8743fe1c99d5a81faf896eb5b9b9d9e4df20bcd3
Amol Jagtap <amoljagtap17@gmail.com>
2020-07-15 22:24:48 +0530

    NG-CARDS: Loop over card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      userName: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      userName: 'mountainskier',
+      content: 'Here is a picture of snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      userName: 'biking456',
+      content: 'This was me biking today',
+    },
+  ];
 }
